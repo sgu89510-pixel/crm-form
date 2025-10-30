c5ea2a33c261a8daf1f5431c8af3a05cb4dd4a85
sgu89510-pixel <sgu89510@gmail.com>
2025-10-30 14:54:27 +0100

    server.py

--- server.py
+++ server.py
@@ -42,5 +42,8 @@ def send_lead():
     except Exception as e:
         return jsonify({"error": str(e), "success": False})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if name == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
