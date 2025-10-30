9ea219533cf2b8dd68945858ab30efa18de9c01f
sgu89510-pixel <sgu89510@gmail.com>
2025-10-30 15:17:42 +0100

    server.py

--- server.py
+++ server.py
@@ -7,8 +7,7 @@ app = Flask(__name__, static_folder='')
 # === СТАТИЧЕСКИЕ ФАЙЛЫ (HTML) ===
 @app.route('/')
 def index():
-    # Просто открывает форму по адресу https://aquatrip.pythonanywhere.com/
-    return send_from_directory('/home/aquatrip', 'lead_form.html')
+    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), 'lead_form.html')
 
 # === ОТПРАВКА ЛИДА В CRM ===
 @app.route('/send_lead', methods=['POST'])
